51cea3a4b33286cae4d765ebf3052b4fae923bad
Tim Younger <tim@webpt.com>
2016-03-22 13:31:21 -0700

    more-code-coverage - spotted some gaps in coverage and filled them. i am not too sure what is happening in the deepest indentations of \EnliteMonolog\Service\MonologServiceFactory::createHandler(), so i have left that uncovered... for now.

--- test/EnliteMonologTest/Service/MonologServiceFactoryTest.php
+++ test/EnliteMonologTest/Service/MonologServiceFactoryTest.php
@@ -8,6 +8,7 @@ namespace EnliteMonologTest\Service;
 
 use EnliteMonolog\Service\MonologOptions;
 use EnliteMonolog\Service\MonologServiceFactory;
+use Monolog\Formatter\LineFormatter;
 use Zend\ServiceManager\ServiceManager;
 
 class MonologServiceFactoryTest extends \PHPUnit_Framework_TestCase
@@ -108,4 +109,163 @@ class MonologServiceFactoryTest extends \PHPUnit_Framework_TestCase
         $factory->createHandler($serviceManager, new MonologOptions($config), array('name' => 'Monolog\Handler\TestHandler', 'args' => ''));
     }
 
+    public function testCreateProcessorFromAnonymousFunction()
+    {
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+
+        $actual = $factory->createProcessor($serviceManager, $expected = function () {
+            
+        });
+        
+        self::assertSame($expected, $actual);
+    }
+
+    public function testCreateProcessorFromServiceName()
+    {
+        $serviceManager = new ServiceManager();
+        $serviceManager->setService('MyProcessor', $expected = function () {
+
+        });
+        
+        $factory = new MonologServiceFactory();
+
+        $actual = $factory->createProcessor($serviceManager, 'MyProcessor');
+
+        self::assertSame($expected, $actual);
+    }
+
+    public function testCreateProcessorFromClassName()
+    {
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+
+        $actual = $factory->createProcessor($serviceManager, '\Monolog\Processor\UidProcessor');
+
+        self::assertInstanceOf('\Monolog\Processor\UidProcessor', $actual);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testCreateProcessorNotExistsClassName()
+    {
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+
+        $factory->createProcessor($serviceManager, '\stdClass');
+    }
+    
+    public function testCreateFormatterFromServiceName()
+    {
+        $serviceManager = new ServiceManager();
+        $serviceManager->setService('MyFormatter', $expected = $this->getMock('\Monolog\Formatter\FormatterInterface'));
+        
+        $factory = new MonologServiceFactory();
+
+        $actual = $factory->createFormatter($serviceManager, 'MyFormatter');
+        
+        self::assertSame($expected, $actual);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testCreateFormatterWithMissingFormatterNameConfig()
+    {
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+
+        $factory->createFormatter($serviceManager, array(
+            
+        ));
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testCreateFormatterNotExistsClassName()
+    {
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+
+        $factory->createFormatter($serviceManager, array(
+            'name' => '\InvalidFormatterClassName',
+        ));
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testCreateFormatterWithInvalidFormatterArgumentConfig()
+    {
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+
+        $factory->createFormatter($serviceManager, array(
+            'name' => '\Monolog\Formatter\LineFormatter',
+            'args' => 'MyArgs',
+        ));
+    }
+    
+    public function testCreateFormatterWithArguments()
+    {
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+
+        $actual = $factory->createFormatter($serviceManager, array(
+            'name' => '\Monolog\Formatter\LineFormatter',
+            'args' => array(
+                'format' => LineFormatter::SIMPLE_FORMAT,
+                'dateFormat' => 'Y-m-d H:i:s',
+            ),
+        ));
+        
+        self::assertInstanceOf('\Monolog\Formatter\LineFormatter', $actual);
+    }
+    
+    public function testCreateFormatterWithoutArguments()
+    {
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+
+        $actual = $factory->createFormatter($serviceManager, array(
+            'name' => '\Monolog\Formatter\LineFormatter',
+        ));
+
+        self::assertInstanceOf('\Monolog\Formatter\LineFormatter', $actual);
+    }
+    
+    public function testCreateLoggerWithProcessor()
+    {
+        $options = new MonologOptions();
+        $options->setProcessors(array(
+            '\Monolog\Processor\UidProcessor',
+        ));
+        
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+        
+        $actual = $factory->createLogger($serviceManager, $options);
+        
+        self::assertInstanceOf('\Monolog\Logger', $actual);
+        self::assertCount(1, $processors = $actual->getProcessors());
+        self::assertInstanceOf('\Monolog\Processor\UidProcessor', $processors[0]);
+    }
+    
+    public function testCreateHandlerWithFormatter()
+    {
+        $serviceManager = new ServiceManager();
+        $factory = new MonologServiceFactory();
+        
+        $actual = $factory->createHandler($serviceManager, new MonologOptions(), array(
+            'name' => '\Monolog\Handler\NullHandler',
+            'formatter' => array(
+                'name' => '\Monolog\Formatter\LineFormatter',
+            ),
+        ));
+        
+        self::assertInstanceOf('\Monolog\Handler\NullHandler', $actual);
+        self::assertInstanceOf('\Monolog\Formatter\LineFormatter', $actual->getFormatter());
+    }
 }
